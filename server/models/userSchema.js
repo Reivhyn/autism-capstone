3b65c9fc29a3362d7bcc7a5cd285dad867e0c87a
Corey Simmons <4casms@gmail.com>
2024-12-30 19:14:28 -0500

    adding user schema

---
+++ server/models/userSchema.js
@@ -0,0 +1,32 @@
+const { mongo, default: mongoose } = require('mongoose')
+
+const User = new mongoose.Schema(
+  {
+    //UNIVERSAL section of schema
+    userType: { type: String, require: true }, // admin ,parent or kid
+    userName: { type: String, required: true, unique: true },
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
+    dob: { type: Date, required: true },
+    email: {
+      type: String,
+      maxLength: 48,
+      required: true,
+      unique: true,
+      match: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+    },
+    password: { type: String, required: true },
+
+    // PARENT section of schema
+    kids: { type: Array },
+
+    // KID section of schema
+    parentUser: { type: String, required: true }, //if parent it value is parent otherwise it is the id of the parent user
+    gamesAccess: { type: Array }, //should list the id of available games
+    chatAccess: { type: Array }, //should list the ids of available chat topics
+    learningAccess: { type: Array }, //should list the ids of available learning topics
+  },
+  { Timestamp: true }
+)
+
+module.exports = mongoose.model('user', User)
