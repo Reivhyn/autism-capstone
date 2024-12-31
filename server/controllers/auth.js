ea9c992064c1bb6ef6ea24dda68e163fd7e63e6a
Kayla Horton <k.horton87038@gmail.com>
2024-12-30 20:16:02 -0500

    added files still need to test

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -44,14 +44,14 @@ router.post('/register', async (req, res) => {
     await newUser.save()
 
     //generate token
-        const token = jwt.sign(
-          //payload
-          { id: newUser._id },
-          //token key
-          JWT_KEY,
-          //epiration
-          { expiresIn: '1 hour' }
-        )
+    const token = jwt.sign(
+      //payload
+      { id: newUser._id },
+      //token key
+      JWT_KEY,
+      //epiration
+      { expiresIn: '1 hour' }
+    )
 
     //TODO SET UP WEB OR SESSION INFO
 
@@ -69,6 +69,37 @@ router.post('/register', async (req, res) => {
   } catch (error) {
     return res.status(500).json({
       message: `${error}`,
+    })
+  }
+})
+
+router.put('/:id', async (req, res) => {
+  try {
+    const { id } = req.params
+
+    // ! check this when corey post schema
+    const updatedEntery = await User.findByIdAndUpdate(id, {
+      userType: req.body.UserType ?? userType,
+      userName: req.body.userName ?? userName,
+      firstName: req.body.firstName ?? firstName,
+      lastName: req.body.lastName ?? lastName,
+      dob: req.body.dob ?? dob,
+      email: req.body.email ?? email,
+      password: req.body.password ?? password,
+      kids: req.body.kids ?? kids,
+      parentUser: req.body.parentUser ?? parentUser,
+      gamesAccess: req.body.gamesAccess ?? gamesAccess,
+      chatAccess: req.body.chatAccess ?? chatAccess,
+      learningAccess: req.body.learningAccess ?? learningAccess,
+    })
+    res.status(200).json({
+      message: `Modified`,
+      updatedEntery,
+    })
+  } catch (err) {
+    console.log(err)
+    res.status(500).json({
+      error: `${err}`,
     })
   }
 })
