aed60e86eefa3e4c775fc1860aae76ed4563e62c
Kayla Horton <k.horton87038@gmail.com>
2025-01-13 18:20:05 -0500

    after merge

--- client/src/components/zzHelpers/helpers.jsx
+++ client/src/components/zzHelpers/helpers.jsx
@@ -45,3 +45,54 @@ export function runSearch(arrayToSearch, searchTerm) {
   //return values  from the map as an array
   return Array.from(uniqueResults.values())
 }
+
+export function validatePasswordCriteria(password) {
+  //GLOBALS
+
+  //determins the min lenght allowed
+  const minLength = 10
+
+  //keeps track of wether meeting the criteria has failed
+  let lengthFail = false
+  let upperCaseFail = false
+  let lowerCaseFail = false
+  let symbolFail = false
+  let numberFail = false
+  // Regular expressions to check for the presence of uppercase, lowercase, number, and allowed symbols
+  const upperCase = /[A-Z]/
+  const lowerCase = /[a-z]/
+  const number = /[0-9]/
+
+  // Allowed symbols !@#$%^&*()_+-=[]{}|;:'",.<>?/~`
+  const symbol = /[!@#$%^&*()_+\-=\[\]{}|;:'",.<>?/~`]/
+
+  // Check if password is at least 10 characters long
+  if (password.length < minLength) {
+    lengthFail = true
+  }
+
+  //check if password meets type criteria
+  if (!upperCase.test(password)) {
+    upperCaseFail = true
+  }
+  if (!lowerCase.test(password)) {
+    lowerCaseFail = true
+  }
+  if (!number.test(password)) {
+    numberFail = true
+  }
+  if (!symbol.test(password)) {
+    symbolFail = true
+  }
+
+  //if any fails trip throw error with what failed to meet criteria
+  if (
+    lengthFail ||
+    upperCaseFail ||
+    lowerCaseFail ||
+    numberFail ||
+    symbolFail
+  ) {
+    return false
+  }
+}
