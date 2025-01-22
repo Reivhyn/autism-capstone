8d9eed1969d6abbb1953a554c40d5ff78ef73256
kh-png <k.horton87038@gmail.com>
2025-01-21 20:42:41 -0500

    fixed save issues with user

--- client/src/components/Portal/Portal.jsx
+++ client/src/components/Portal/Portal.jsx
@@ -30,7 +30,7 @@ const Portal = () => {
   const [pageToDisplay, setPageToDisplay] = useContext(ptdContext)
   const [userData, setUserData] = useContext(userDataContext)
   const [allActivities, setAllActivities] = useState('')
-  const [kidsOfParent, setKidsOfParent] = useContext(KidsOfParentContext)
+  const [kidsOfParent, setKidsOfParent] = useState('')
   const [allUsers, setAllUsers] = useState('')
 
   //* FUNCTIONS
@@ -120,40 +120,7 @@ const Portal = () => {
     </>
   )
 
-  //render parent portal
-  if (pageToDisplay === 'parent') {
-    return (
-      <>
-        <SiteTitle />
-        <h2>{`${pageToDisplay.toUpperCase()} PORTAL`}</h2>
 
-        <div className="portalListWrapper">
-          {/* all users list */}
-          {kidsOfParent ? (
-            <PortalList itemsToList={kidsOfParent} listType={'kids'} />
-          ) : (
-            'Fetching Data'
-          )}
-
-          {/* all games list */}
-          {allActivities ? (
-            <PortalList itemsToList={allActivities} listType={'games'} />
-          ) : (
-            'Fetching Data'
-          )}
-
-          {/* all learning list */}
-          {allActivities ? (
-            <PortalList itemsToList={allActivities} listType={'learning'} />
-          ) : (
-            'Fetching Data'
-          )}
-        </div>
-
-        <Footer />
-      </>
-    )
   }
-}
 
 export default Portal
