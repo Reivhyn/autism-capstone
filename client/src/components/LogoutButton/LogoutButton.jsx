2d9c4376e187a2ac98770b47a54988220f23d6bb
cscott933 <cscott933@gmail.com>
2025-01-26 21:23:41 -0500

    simple footer, searchlogout positioned

--- client/src/components/LogoutButton/LogoutButton.jsx
+++ client/src/components/LogoutButton/LogoutButton.jsx
@@ -1,23 +1,38 @@
-import React from 'react'
-import './logout.css';
+import React from 'react';
+import { useTheme, Button } from '@mui/material';
 
 const LogoutButton = () => {
-  //* FUNCTIONS
-  // logs out and deletes cookie and sessioon storage
+  const theme = useTheme();
+
+  // Logs out and deletes cookies and session storage
   const logout = () => {
-    document.cookie = `authToken=; Expires=Thu, 01 Jan 1970 00:00:01 GMT;`
-    sessionStorage.clear()
-    window.location.reload(true)
-  }
+    document.cookie = `authToken=; Expires=Thu, 01 Jan 1970 00:00:01 GMT;`;
+    sessionStorage.clear();
+    window.location.reload(true);
+  };
 
-  //* RENDER
   return (
-    <>
-      <button onClick={logout} className="logOutButton">
-        Logout
-      </button>
-    </>
-  )
-}
+    <Button
+      onClick={logout}
+      sx={{
+        position: 'absolute',
+        right: '10px',
+        top: '10px',
+        padding: '10px 20px',
+        backgroundColor: theme.palette.primary.main,
+        color: theme.palette.primary.contrastText,
+        borderRadius: '12px',
+        fontSize: '13px',
+        zIndex: 1000,
+        transition: 'background-color 0.3s ease',
+        '&:hover': {
+          backgroundColor: theme.palette.primary.dark,
+        },
+      }}
+    >
+      Logout
+    </Button>
+  );
+};
 
-export default LogoutButton
+export default LogoutButton;
