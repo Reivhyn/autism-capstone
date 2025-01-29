ff2b8be54f808e86d81c8a7762d139ac4e6d7ace
cscott933 <cscott933@gmail.com>
2025-01-29 09:48:38 -0500

    deleted extra dropmenu & edited search bar

--- client/src/components/SearchBar/SearchBar.jsx
+++ client/src/components/SearchBar/SearchBar.jsx
@@ -8,13 +8,15 @@ import React, { useState } from 'react';
 import { TextField, InputAdornment, IconButton } from '@mui/material';
 import SearchIcon from '@mui/icons-material/Search';
 import './SearchBar.css';
+import { useTheme } from '@mui/material/styles';
 
 const SearchBar = ({ setSearchTerm }) => {
 const [inputValue, setInputValue] = useState('');
+const theme = useTheme();
 
   const handleSubmit = (e) => {
-    console.log(e)
-    console.log(inputValue)
+    console.log(e) // Can take out
+    console.log(inputValue) // Can take out
     e.preventDefault();
     setSearchTerm(inputValue); // Update search term in parent
   };
@@ -28,6 +30,19 @@ const [inputValue, setInputValue] = useState('');
         value={inputValue}
         onChange={(e) => setInputValue(e.target.value)} // Update local state
         InputProps={{
+          sx: {
+            backgroundColor: theme.palette.background.paper, // Use theme background
+            color: theme.palette.text.primary, // Text color from theme
+            '& .MuiOutlinedInput-notchedOutline': {
+              borderColor: theme.palette.primary.main, // Border color from theme
+            },
+            '&:hover .MuiOutlinedInput-notchedOutline': {
+              borderColor: theme.palette.secondary.main, // Change on hover
+            },
+            '&.Mui-focused .MuiOutlinedInput-notchedOutline': {
+              borderColor: theme.palette.primary.dark, // Focus border color
+            },
+          },
           endAdornment: (
             <InputAdornment position="end">
               <IconButton
