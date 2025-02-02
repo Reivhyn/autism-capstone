c5ca1428b7581168a75437a2f478154ae5ea3302
kh-png <k.horton87038@gmail.com>
2025-02-02 15:27:24 -0500

    after merge

--- client/src/components/Logo/Logo.jsx
+++ client/src/components/Logo/Logo.jsx
@@ -2,6 +2,6 @@ import React from 'react'
 import './logo.css'
 export default function Logo() {
   return (
-    <img src="src\assets\SiteLogo.png" alt="Site logo" />
+    <img className='logo' src="src\assets\SiteLogo.png" alt="Site logo" />
   )
 }
