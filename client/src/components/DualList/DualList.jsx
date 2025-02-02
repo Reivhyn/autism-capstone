d91ab716c67fe9068377ee8c7318d8aec519aa45
cscott933 <cscott933@gmail.com>
2025-02-02 13:43:58 -0500

    columns and box response

--- client/src/components/DualList/DualList.jsx
+++ client/src/components/DualList/DualList.jsx
@@ -309,7 +309,10 @@ const DualList = ({
               '&::-webkit-scrollbar': {
                 display: 'none', // Hide scrollbar for Chrome, Safari, and Edge
               },
-            }}
+              '@media (max-width: 320px)': {
+                flexDirection: 'column',  // Adjust width for small devices
+              }
+              }}
           >
             {/* Available list */}
             <Box sx={{ width: '45%' }}>
